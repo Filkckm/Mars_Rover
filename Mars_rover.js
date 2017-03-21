f589de8ce3b05978b7c957885ac1e88f47cf382e
Filkckm <phillip.kickum@myebs.de>
2017-03-21 13:58:21 +0100

    SendIt function first try
    need to find a way to make a string like "fffrrblbf" readable and executable

--- Mars_rover.js
+++ Mars_rover.js
@@ -1,22 +1,108 @@
-var Rover = {
-  position: [0,0],
-  direction: 'N'
-}
+var rover = {
+  position: [0,0], 
+  direction: "N"
+};
+var sequence = ""
 
-function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
+function goForward(rover){
+  switch(rover.direction){
+    case "N":
       rover.position[1]++
       break;
-    case 'S':
-      rover.position[0]--
+    case "E":
+      rover.position[0]++
       break;
-    case 'W':
+    case "S":
       rover.position[1]--
       break;
-  }
+    case "W":
+      rover.position[0]--
+      break;
+  };
+
+  document.write("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]. ")
 }
-goForward(myRover);
+//goForward(rover);
+
+function goBackward(rover){
+  switch (rover.direction){
+    case "N":
+      rover.position [1]--
+      break;
+    case "E":
+      rover.position[0]--
+      break;
+    case "S":
+      rover.position[1]++
+      break;
+    case "W":
+      rover.position[0]++
+      break;
+     };
+ document.write("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]. ")
+}
+//goBackward(rover);
+
+function turnLeft(rover){
+  switch (rover.direction){
+      case"N":
+        rover.direction = "W"
+        break;
+      
+      case"E":
+        rover.direction = "N"
+        break;
+      
+      case"S":
+        rover.direction = "E"
+        break;
+      
+      case"W":
+        rover.direction = "S"
+        break;
+       };
+  document.write("New Rover Direction: " + rover.direction + ". ")
+}
+//turnLeft(rover)
+
+function turnRight(rover){
+  switch (rover.direction){
+      case"N":
+        rover.direction = "E"
+        break;
+      
+      case"E":
+        rover.direction = "S"
+        break;
+      
+      case"S":
+        rover.direction = "W"
+        break;
+      
+      case"W":
+        rover.direction = "N"
+        break;
+       };
+  document.write("New Rover Direction: " + rover.direction + ". ")
+}
+//turnRight(rover)
+
+function SendIt (sequence){
+  var sequence = prompt("please enter a command")
+  switch(sequence){
+    case "f":
+      goForward(rover)
+      break;
+      case "b":
+      goBackward(rover)
+      break;
+      case "l":
+      turnLeft(rover)
+      break;
+    case "r":
+      turnRight(rover)
+      break;
+                 }
+}
+
+SendIt(sequence)
